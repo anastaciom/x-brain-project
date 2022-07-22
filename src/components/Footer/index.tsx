bd2951447e804ddc44a1a6f4cfb82bd17b3f99ec
Anastacio Menezes <anastaciomenezes@outlook.com>
2022-07-21 23:05:12 -0300

    feat: create form and UI tweaks

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,41 @@
 import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+  name: string;
+  email: string;
+  sexo: string;
+};
 export default function Footer() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   return (
     <Box py={8}>
       <Typography variant="h5" color={"#333"} pb={1}>
         Dados do cliente
       </Typography>
       <Divider />
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("name", { required: true })} />
+        {errors.name && <span>This field is required</span>}
+        <input {...register("email", { required: true })} />
+        {errors.email && <span>This field is required</span>}
+        <select {...register("sexo", { required: true })} defaultValue="">
+          <option value="" disabled>
+            Selecione
+          </option>
+          <option value="Masculino">Masculino</option>
+          <option value="Feminino">Feminino</option>
+        </select>
+        {errors.sexo && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
     </Box>
   );
 }
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,28 +1,47 @@
-import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 import { IData } from "../../data/types";
-import {formatThePrice} from "../../utils/formatThePrice";
-
+import { formatThePrice } from "../../utils/formatThePrice";
+import RemoveIcon from "../../assets/icons/baseline-remove-24px.svg";
+import AddIcon from "../../assets/icons/baseline-add-24px.svg";
 interface ProductCardProps {
   product: IData;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const [showProductDetails, setShowProductDetails] = useState(false);
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3} height={420}>
+    <Grid item xs={12} sm={4} md={3} lg={3} height={420}>
       <Card
+        onMouseEnter={() => setShowProductDetails(true)}
+        onMouseLeave={() => setShowProductDetails(false)}
         sx={{
+          position: "relative",
           height: "100%",
           maxHeight: "100%",
-          // transform: "scale(0.95)",
-          cursor: "pointer",
+          transform: "scale(0.98)",
           width: "100%",
           maxWidth: "100%",
-          padding: 2,
           boxShadow: "none",
-          justifyContent: "center",
+          justifyContent: "flex-start",
           alignItems: "center",
           display: "flex",
           flexDirection: "column",
+          transition: "all ease-in 0.2s",
+          "&:hover": {
+            transform: "scale(1)",
+            boxShadow:
+              "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
+          },
         }}
       >
         <CardMedia
@@ -33,41 +52,127 @@ export default function ProductCard({ product }: ProductCardProps) {
           component="img"
           height="140"
           image={product.image}
-          alt="green iguana"
+          alt={product.productTitle}
         />
-        <CardContent
-          sx={{
-            height: "50%",
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "flex-start",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <Typography
-            gutterBottom
-            variant="body1"
-            color={"GrayText"}
-            component="div"
+        {!showProductDetails ? (
+          <CardContent
+            sx={{
+              width: "100%",
+              height: "50%",
+              justifyContent: "center",
+              alignItems: "flex-start",
+              display: "flex",
+              flexDirection: "column",
+              gap: 0.5,
+            }}
           >
-            {product.productTitle}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            fontWeight={"bold"}
-            fontSize={20}
+            <Typography
+              gutterBottom
+              variant="body1"
+              color={"GrayText"}
+              component="div"
+            >
+              {product.productTitle}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              fontWeight={"bold"}
+              fontSize={20}
+            >
+              {formatThePrice(product.price)}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.productDescription.split(" - ")[0]}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {product.productDescription.split(" - ")[1]}
+            </Typography>
+          </CardContent>
+        ) : (
+          <Box
+            width={"100%"}
+            height={"100%"}
+            position={"absolute"}
+            display="flex"
+            alignItems={"center"}
+            justifyContent={"flex-end"}
+            flexDirection="column"
           >
-            {formatThePrice(product.price)}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product.productDescription.split(" - ")[0]}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product.productDescription.split(" - ")[1]}
-          </Typography>
-        </CardContent>
+            <Box
+              width={"100%"}
+              height={"70%"}
+              sx={{ background: "#f1f1f188", cursor: "pointer" }}
+            >
+              <Box
+                display="flex"
+                alignItems={"flex-start"}
+                flexDirection="column"
+                p={2}
+                gap={0.5}
+                width={"100%"}
+                height={"50%"}
+              >
+                <Typography
+                  gutterBottom
+                  variant="body1"
+                  color={"GrayText"}
+                  component="div"
+                >
+                  {product.productTitle}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  fontWeight={"bold"}
+                  fontSize={20}
+                >
+                  {formatThePrice(product.price)}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.productDescription.split(" - ")[0]}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {product.productDescription.split(" - ")[1]}
+                </Typography>
+              </Box>
+
+              <Box
+                width={"100%"}
+                height={"50%"}
+                display={"flex"}
+                alignItems="center"
+                flexDirection={"column"}
+                justifyContent={"space-evenly"}
+              >
+                <Box
+                  width={"100%"}
+                  height={"50%"}
+                  bgcolor={"blue"}
+                  display={"flex"}
+                  alignItems="center"
+                  justifyContent={"space-around"}
+                >
+                  <img src={RemoveIcon} alt="remover um item do carrinho" />
+                  <TextField
+                    sx={{ width: 100 }}
+                    id="outlined-number"
+                    label="Qtd."
+                    type="number"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                  <img src={AddIcon} alt="adicionar mais um item no carrinho" />
+                </Box>
+
+                <Button variant="contained" sx={{ width: "80%" }}>
+                  Adicionar
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        )}
       </Card>
     </Grid>
   );
